7c0f83dfea6259a43ed6b2f770e67392009b42d0
Bayu <bayua9731@gmail.com>
2023-05-10 21:24:41 +0700

    Initial commit

---
+++ app/src/main/java/com/anmp/projekuts/view/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.anmp.projekuts.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.anmp.projekuts.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
