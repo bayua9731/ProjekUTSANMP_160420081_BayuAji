e2f772a82b9f45e622a713eee85f5033d467291f
Bayu Aji Hamengku Rahmad <75962248+bayua9731@users.noreply.github.com>
2023-05-12 09:02:09 +0700

    PHP FILE

---
+++ search.php
@@ -0,0 +1,227 @@
+<?php 
+    $q=$_GET["query"];
+    $array=array(
+        array(
+            "id"=>"1",
+            "nama"=>"Kost AM 12",
+            "alamat"=>"Jl. Tenggilis Mejoyo Blok AM No.12, Kali Rungkut, Kec. Rungkut, Surabaya, Jawa Timur 60293",
+            "harga"=>"500000",
+            "photo"=>
+                "https://lh5.googleusercontent.com/p/AF1QipMqNTAyOMvaqGJCutFaQgNscRlhyiqbdSqwNA3y=w253-h337-k-no",
+            "pemilik"=>"Agus",
+            "kontak"=>"123123123",
+            "jarak"=>"1",
+            "kategori"=>"Putri",
+            "deskripsi"=>implode(",",array(
+                "Kamar Mandi Dalam",
+                "Laundry",
+                "Area Parkiran Luas",
+                "WIFI",
+                "Token PLN",
+                "Dapur",
+                "Dekat Ubaya"
+            ))
+        ),
+        array(
+            "id"=>"2",
+            "nama"=>"Kost AM-34",
+            "alamat"=>"Rungkut Mejoyo Selatan AM-34, Kali Rungkut, Rungkut, Surabaya, East Java 60293",
+            "harga"=>"450000",
+            "photo"=>
+                "https://lh3.googleusercontent.com/p/AF1QipNy5HH5_xByTTDG7C_3jP6IjWeroeeffHtlS-Tc=s1360-w1360-h1020"
+            ,
+            "pemilik"=>"Baru",
+            "kontak"=>"123123123",
+            "jarak"=>"1.5",
+            "kategori"=>"Putri",
+            "deskripsi"=>implode(",",array(
+                "Kamar Mandi Dalam",
+                "Laundry",
+                "Area Parkiran Luas",
+                "WIFI",
+                "Token PLN",
+                "Dapur",
+                "Dekat Ubaya"
+            ))
+        ),
+        array(
+            "id"=>"3",
+            "nama"=>"KOST Putri Ubaya",
+            "alamat"=>"Jl. Tenggilis Mejoyo Blk. AN No.26, Kali Rungkut, Kec. Rungkut, Surabaya, Jawa Timur 60292",
+            "harga"=>"700000",
+            "photo"=>"https://lh3.googleusercontent.com/p/AF1QipMVDFre6dYmNENYEZ8550pd9ZlQkhLMLxZu8jci=s1360-w1360-h1020"
+            ,
+            "pemilik"=>"Cantik",
+            "kontak"=>"123123123",
+            "jarak"=>"1.5",
+            "kategori"=>"Putri",
+            "deskripsi"=>implode(",",array(
+                "Kamar Mandi Dalam",
+                "Laundry",
+                "Area Parkiran Luas",
+                "WIFI",
+                "Token PLN",
+                "Dapur",
+                "Dekat Ubaya"
+            ))
+        ),
+        array(
+            "id"=>"4",
+            "nama"=>"Kost AH 21 Ubaya",
+            "alamat"=>"Jl. Rungkut Mejoyo Utara I Blok AH No.21, Kali Rungkut, Rungkut, Surabaya, East Java 60293",
+            "harga"=>"1000000",
+            "photo"=>
+                "https://lh5.googleusercontent.com/p/AF1QipO4Jsuva_7pnMwBaNhRleKbXxRaXK-aWuEzzwxU=w325-h218-n-k-no"
+            ,
+            "pemilik"=>"Dari",
+            "kontak"=>"123123123",
+            "jarak"=>"2.1",
+            "kategori"=>"Putri",
+            "deskripsi"=>implode(",",array(
+                "Kamar Mandi Dalam",
+                "Laundry",
+                "Area Parkiran Luas",
+                "WIFI",
+                "Token PLN",
+                "Dapur",
+                "Dekat Ubaya"
+            ))
+        ),
+        array(
+            "id"=>"5",
+            "nama"=>"Kost Depan Ubaya KK 4",
+            "alamat"=>"KK, Jl. Raya Tenggilis Mejoyo, Kali Rungkut, Kec. Rungkut, Surabaya, Jawa Timur 60293",
+            "harga"=>"750000",
+            "photo"=>"https://lh3.googleusercontent.com/p/AF1QipNPVCBhsDba7UmV9oRt-msi30eMkXY4603sva4P=s1360-w1360-h1020"
+            ,
+            "pemilik"=>"Elok",
+            "kontak"=>"123123123",
+            "jarak"=>"1.8",
+            "kategori"=>"Putri",
+            "deskripsi"=>implode(",",array(
+                "Kamar Mandi Dalam",
+                "Laundry",
+                "Area Parkiran Luas",
+                "WIFI",
+                "Token PLN",
+                "Dapur",
+                "Dekat Ubaya"
+            ))
+        ),
+        array(
+            "id"=>"6",
+            "nama"=>"Kost Masbro Ubaya",
+            "alamat"=>"raya tenggilis mejoyo Ai 33 - 34, Kali Rungkut, Kec. Rungkut, Surabaya, Jawa Timur 60293",
+            "harga"=>"200000",
+            "photo"=>"https://lh3.googleusercontent.com/p/AF1QipM8eK3TrAz00-CcUDRrLNhHzfktSZTT4241T7K-=s1360-w1360-h1020"
+            ,
+            "pemilik"=>"Fauzan",
+            "kontak"=>"123123123",
+            "jarak"=>"3",
+            "kategori"=>"Putra",
+            "deskripsi"=>implode(",",array(
+                "Kamar Mandi Dalam",
+                "Laundry",
+                "Area Parkiran Luas",
+                "WIFI",
+                "Token PLN",
+                "Dapur",
+                "Dekat Ubaya"
+            ))
+        ),
+        array(
+            "id"=>"7",
+            "nama"=>"KOSTKU RESIDENCE",
+            "alamat"=>"Jl. Tenggilis Mejoyo Utara II, Tenggilis Mejoyo, Surabaya, East Java 60293",
+            "harga"=>"800000",
+            "photo"=>"https://lh3.googleusercontent.com/p/AF1QipMBr6kV0DpCc5yFPl8OuZ_4aASaqCFj-4dOlgTC=s1360-w1360-h1020"
+            ,
+            "pemilik"=>"Garuda",
+            "kontak"=>"123123123",
+            "jarak"=>"2.5",
+            "kategori"=>"Putra",
+            "deskripsi"=>implode(",",array(
+                "Kamar Mandi Dalam",
+                "Laundry",
+                "Area Parkiran Luas",
+                "WIFI",
+                "Token PLN",
+                "Dapur",
+                "Dekat Ubaya"
+            ))
+        ),
+        array(
+            "id"=>"8",
+            "nama"=>"Kost Putra AA-4",
+            "alamat"=>"Jl. Raya Tenggilis Mejoyo, AA - 4, Kali Rungkut, Kec. Rungkut, Surabaya, Jawa Timur 60293",
+            "harga"=>"1250000",
+            "photo"=>"https://lh3.googleusercontent.com/p/AF1QipNqxODtUZzHVB_-j-xXVMe395GCasmw4Z-JwDvp=s1360-w1360-h1020"
+            ,
+            "pemilik"=>"Hari",
+            "kontak"=>"123123123",
+            "jarak"=>"3",
+            "kategori"=>"Putra",
+            "deskripsi"=>implode(",",array(
+                "Kamar Mandi Dalam",
+                "Laundry",
+                "Area Parkiran Luas",
+                "WIFI",
+                "Token PLN",
+                "Dapur",
+                "Dekat Ubaya"
+            ))
+        ),
+        array(
+            "id"=>"9",
+            "nama"=>"Kost Putra AE 27 (UBAYA)",
+            "alamat"=>" Blok AE, Rungkut Mejoyo Utara V No.27, Tenggilis Mejoyo, Kec. Rungkut, Surabaya, Jawa Timur 60293",
+            "harga"=>"500000",
+            "photo"=>"https://lh3.googleusercontent.com/p/AF1QipNUxsoHccZ8LC3s_NAN2NvsAFdytk2CtMNJt5Zg=s1360-w1360-h1020"
+            ,
+            "pemilik"=>"Jauhari",
+            "kontak"=>"123123123",
+            "jarak"=>"2.5",
+            "kategori"=>"Putra",
+            "deskripsi"=>implode(",",array(
+                "Kamar Mandi Dalam",
+                "Laundry",
+                "Area Parkiran Luas",
+                "WIFI",
+                "Token PLN",
+                "Dapur",
+                "Dekat Ubaya"
+            ))
+        ),
+        array(
+            "id"=>"10",
+            "nama"=>"KOS PUTRA UBAYA AF 33",
+            "alamat"=>"TENGGILIS MEJOYO UTARA IV AF 33, Kali Rungkut, Kec. Rungkut, Surabaya, Jawa Timur 60293",
+            "harga"=>"500000",
+            "photo"=>
+                "https://lh3.googleusercontent.com/p/AF1QipM7T9e2NublrJguMCLaAjlrbZ5gH-oaqwPPdx-e=s1360-w1360-h1020"
+            ,
+            "pemilik"=>"Kualanamu",
+            "kontak"=>"123123123",
+            "jarak"=>"2.5",
+            "kategori"=>"Putra",
+            "deskripsi"=>implode(",",array(
+                "Kamar Mandi Dalam",
+                "Laundry",
+                "Area Parkiran Luas",
+                "WIFI",
+                "Token PLN",
+                "Dapur",
+                "Dekat Ubaya"
+            ))
+        ),
+    );
+    $hasil=[];
+    foreach($array as $val){
+        $cekfull=strtolower($val["nama"]);
+        $q=strtolower($q);
+        if (strpos($cekfull, $q)!== false){
+$hasil[]=$val;
+        }
+    }
+    echo json_encode($hasil);
+?>
